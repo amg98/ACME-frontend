c72dfac36a78d72f33cddf9856ea9e737ef13afc
AMG <andresmargar98@gmail.com>
2021-04-13 22:25:36 +0200

    fix: production server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ACME-frontend
+
+[![Deploy on Heroku](https://github.com/amg98/ACME-frontend/actions/workflows/deploy.yml/badge.svg)](https://github.com/amg98/ACME-frontend/actions/workflows/deploy.yml)
+
+http://acmeexplorer-frontend.herokuapp.com/
--- src/productionServer.js
+++ src/productionServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const STATIC = path.resolve(__dirname, "..", "dist");
+const STATIC = path.resolve(__dirname, "..", "dist/acme-frontend");
 const INDEX = path.resolve(STATIC, "index.html");
 
 app.use(express.static(STATIC));
