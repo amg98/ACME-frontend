6855e397839196a2afe4691dbe8c6597d126966f
AMG <andresmargar98@gmail.com>
2021-05-13 10:57:27 +0200

    feat: level C search bar done

--- src/app/components/finder/finder.component.ts
+++ src/app/components/finder/finder.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core"
+import { MatSnackBar } from "@angular/material/snack-bar"
 import { TranslatableComponent } from "@components/translatable/translatable.component"
 import { TranslatorService } from "@services/translator.service"
+import { TripsService } from "@services/trips.service"
 
 @Component({
     selector: "app-finder",
@@ -9,7 +11,22 @@ import { TranslatorService } from "@services/translator.service"
 })
 export class FinderComponent extends TranslatableComponent {
 
-    constructor(translator: TranslatorService) {
+    keyword = ""
+
+    constructor(translator: TranslatorService,
+        private tripsService: TripsService,
+        private snackbar: MatSnackBar) {
         super(translator)
+    }
+
+    search(): void {
+        if(this.keyword === "") {
+            this.snackbar.open(this.msg["no-keyword"], this.msg.close, {
+                duration: 5000,
+                panelClass: ["alert-info"]
+            })
+            return
+        }
+        this.tripsService.anonymousSearch(this.keyword)
     }
 }
