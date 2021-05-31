5d446eefef7f6ee6d4ba99f1c14f30609bbafa6d
favsol26 <favsol26@gmail.com>
2021-05-31 16:48:43 +0200

    apply for a trip test

--- src/app/components/trips/apply-for-a-trip.spec.ts
+++ src/app/components/trips/apply-for-a-trip.spec.ts
@@ -1,27 +1,37 @@
-import { Trip } from "src/app/models/Trip"
+// import { Trip } from "src/app/models/Trip" 
+// import { HttpClientTestingModule } from "@angular/common/http/testing"
+// import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing"
+// import { MatDialogModule } from "@angular/material/dialog"
+// import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+// import { MatSnackBarModule } from "@angular/material/snack-bar"
+// import { ApplicationsService, GetManagerApplicationsData } from "@services/applications.service"
+// import { TranslatorService } from "@services/translator.service"
+// import { ApplicationStatus } from "src/app/models/Application"
+// import { Trip } from "src/app/models/Trip"
 
-describe("apply-for-a-trip", () => {
+// describe("apply-for-a-trip", () => {
 
-    const tripID = "123"
-    const sampleTrip: Trip = {
-        _id: tripID,
-        ticker: "210510-IMUZ",
-        title: "Trip title",
-        requirements: ["R1", "R2", "R3"],
-        startDate: "2021-05-20T12:46:31.290Z",
-        endDate: "2021-06-20T12:46:31.290Z",
-        pictures: ["https://images.io/image.jpg"],
-        cancelReason: "",
-        isCancelled: false,
-        isPublished: true,
-        price: 120,
-        stages: [
-            {
-                title: "Stage 1",
-                description: "Stage 1 description",
-                price: 120,
-            }
-        ],
-    }
+//     const tripID = "123"
+//     const sampleTrip: Trip = {
+//         _id: tripID,
+//         ticker: "210510-IMUZ",
+//         title: "Trip title",
+//         requirements: ["R1", "R2", "R3"],
+//         startDate: "2021-05-20T12:46:31.290Z",
 
-})
+//         endDate: "2021-06-20T12:46:31.290Z",
+//         pictures: ["https://images.io/image.jpg"],
+//         cancelReason: "",
+//         isCancelled: false,
+//         isPublished: true,
+//         price: 120,
+//         stages: [
+//             {
+//                 title: "Stage 1",
+//                 description: "Stage 1 description",
+//                 price: 120,
+//             }
+//         ],
+//     }
+
+// })
